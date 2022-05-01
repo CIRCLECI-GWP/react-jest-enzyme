98f5c242c890a7c336788c5bbd9730824d13d0e0
Dominic Motuka <dominic.motuka@gmail.com>
2022-05-01 11:15:32 +0300

    Add a test to ensure that the selectedSubreddit prop is always passed to the Picker component.
    Signed-off-by: Dominic Motuka <dominic.motuka@gmail.com>

--- src/containers/__tests__/App.test.js
+++ src/containers/__tests__/App.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import toJson from "enzyme-to-json";
 import { App } from "../App";
+import Picker from "../../components/Picker";
 
 describe("App", () => {
   it("renders without crashing given the required props", () => {
@@ -14,4 +15,17 @@ describe("App", () => {
     const wrapper = shallow(<App {...props} />);
     expect(toJson(wrapper)).toMatchSnapshot();
   });
+});
+
+it("sets the selectedSubreddit prop as the `value` prop on the Picker component", () => {
+  const props = {
+    isFetching: false,
+    dispatch: jest.fn(),
+    selectedSubreddit: "reactjs",
+    posts: [],
+  };
+  const wrapper = shallow(<App {...props} />);
+  // Query for the Picker component in the rendered output
+  const PickerComponent = wrapper.find(Picker);
+  expect(PickerComponent.props().value).toBe(props.selectedSubreddit);
 });
