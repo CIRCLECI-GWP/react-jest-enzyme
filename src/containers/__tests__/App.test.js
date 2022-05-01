7c742763d22f05e71b0263543b40f4c34f7fd506
Dominic Motuka <dominic.motuka@gmail.com>
2022-05-01 11:19:24 +0300

    Test handleRefreshClick dispatches the correct actions
    Signed-off-by: Dominic Motuka <dominic.motuka@gmail.com>

--- src/containers/__tests__/App.test.js
+++ src/containers/__tests__/App.test.js
@@ -3,6 +3,7 @@ import { shallow } from "enzyme";
 import toJson from "enzyme-to-json";
 import { App } from "../App";
 import Picker from "../../components/Picker";
+import * as actions from "../../actions";
 
 describe("App", () => {
   it("renders without crashing given the required props", () => {
@@ -39,4 +40,29 @@ it("renders the Refresh button when the isFetching prop is false", () => {
   };
   const wrapper = shallow(<App {...props} />);
   expect(wrapper.find("button").length).toBe(1);
+});
+
+it("handleRefreshClick dispatches the correct actions", () => {
+  const props = {
+    isFetching: false,
+    dispatch: jest.fn(),
+    selectedSubreddit: "reactjs",
+    posts: [],
+  };
+  // Mock event to be passed to the handleRefreshClick function
+  const mockEvent = {
+    preventDefault: jest.fn(),
+  };
+  // Mock the actions we expect to be called
+  actions.invalidateSubreddit = jest.fn();
+  actions.fetchPostsIfNeeded = jest.fn();
+
+  const wrapper = shallow(<App {...props} />);
+  // Call the function on the component instance, passing the mock event
+  wrapper.instance().handleRefreshClick(mockEvent);
+
+  expect(mockEvent.preventDefault).toHaveBeenCalled();
+  expect(props.dispatch.mock.calls.length).toBe(3);
+  expect(actions.invalidateSubreddit.mock.calls.length).toBe(1);
+  expect(actions.fetchPostsIfNeeded.mock.calls.length).toBe(2);
 });
